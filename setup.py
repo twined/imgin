3ac0551f257d23699832ca9f6a34ce17b20a933d
Trond Mjøen <trond@twined.net>
2014-11-22 23:16:58 +0100

    - adds django_rq requirement

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ from setuptools import find_packages
 
 setup(
     name='imgin',
-    version='1.0.0',
+    version='1.0.1',
     author=u'Twined',
     author_email='www.twined.net',
     packages=find_packages(),
     include_package_data=True,
     url='http://github.com/twined/imgin',
     license='Do what thou wilt.',
-    description='Image manipulation for twined apps',
+    description='Image manipulation for Twined apps',
     long_description=open('README.md').read(),
     install_requires=[
         "mock-django",
         "nose",
         "Django==1.7.1",
         "Pillow",
+        "django_rq",
     ],
     zip_safe=False,
 )
